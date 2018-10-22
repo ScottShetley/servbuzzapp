9b79b5add3c2a6fd89905eedd530cf4b4944e1e0
VinnyWr <vincent88w@gmail.com>
2018-10-22 15:28:50 -0700

    fixed typo

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,9 @@
 import Home from "./home";
 import Header from "./header";
 import Login from "./login";
+import Home from "./home";
+import Header from "./header";
+import Login from "./login";
 import Logout from "./logout";
 
 export { Home, Header, Login, Logout };
