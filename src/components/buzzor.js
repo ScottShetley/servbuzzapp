a81c9e8a8a6532011a4c5d630f72cce066eb198e
VinnyWr <vincent88w@gmail.com>
2018-11-01 12:41:47 -0700

    notification onClick

--- src/components/buzzor.js
+++ src/components/buzzor.js
@@ -1,10 +1,52 @@
 import React from "react";
+import Push from "../../node_modules/push.js";
+
+// check for dynamically importing via module
 
 const Buzzor = props => (
   <div>
     <h1 className="cover-heading">buzzor works</h1>
-    <button className="btn tbn-primary">Buzz</button>
+    <button
+      // className="btn btn-primary"
+      // onClick={this.handleNotificationChange}
+      onClick={() => {
+        Push.create("Buzzor Works");
+      }}
+    >
+      Buzz
+    </button>
   </div>
 );
 
 export default Buzzor;
+
+// Was stateful, changed to the stateless (above) on Clinton's recommendation
+
+// class Buzzor extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       // userHasClicked: null
+//     };
+//   }
+
+//   // handleNotificationChange = event => {
+//   //   this.setState({ userHasClicked: event.target.value });
+//   // };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1 className="cover-heading">buzzor works</h1>
+//         <button
+//           // className="btn btn-primary"
+//           // onClick={this.handleNotificationChange}
+//           onClick={Push.create("Buzzor Works")}
+//         >
+//           Buzz
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+// export default Buzzor;
