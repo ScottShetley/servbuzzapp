d6b60ab4a6e56469fb59baf076ff1c02424f325a
VinnyWr <vincent88w@gmail.com>
2018-11-26 12:18:31 -0700

    demoDayMerge

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
+store.dispatch(FetchBuzzes());
 setInterval(function() {
   store.dispatch(FetchBuzzes());
   console.log("fetching buzzes");
